a321999399ad3e49c022b824783de8185349e738
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-28 17:11:30 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -8,6 +8,7 @@
         return $("<button>",{text: title}).appendTo(parentNode||h).click(func);
     }
     $("<h1>",{text:"絵文字の肌の色差分を取得"}).appendTo(h);
+    $("<div>",{text:"出力内容は、クリックしただけでコピーされます。"}).appendTo(h);
     var colorName = [
         "標準",
         "明るい肌色",
@@ -71,14 +72,11 @@
         h_emoji.empty();
         getEmojiColors(str).forEach(function(v,i){
             var line = $("<div>").appendTo(h_emoji);
-            addBtn("copy",function(){
-                yaju1919.copy(v);
-            },line);
             yaju1919.addInputText(line,{
                 title: colorName[i],
-                value: v
+                value: v,
+                readonly: true,
             });
-            line.find("div").css({display: "inline-block"});
         });
     }
     function getEmojiColors(str){
@@ -97,51 +95,49 @@
         id2: "hide",
         title: "おんｊ動くAA作成ツール",
     });
-    addBtn("copy",function(){
-        if(result) yaju1919.copy(result);
-    },$("#hide"));
+    var h_result = $("<div>").appendTo(h);
+    function showResult(array){
+        var str = "@aaa:0.1\n" + array.join('\n@@@\n');
+        $("<div>").text("現在の文字数:"+str.length).appendTo(h_result.empty());
+        yaju1919.addInputText(h_result,{
+            title: "出力",
+            value: str,
+            readonly: true,
+        });
+    }
     addBtn("簡単作成",function(){
         var width = 5, height = 5;
         var ar = getEmojiColors(input()).slice(1);
-        result = "@aaa:0.1\n" + ar.concat(ar.slice(1,-1).reverse()).map(function(v){
+        showResult(ar.concat(ar.slice(1,-1).reverse()).map(function(v){
             return new Array(height).fill().map(function(){
                 return yaju1919.repeat(v, width);
             }).join('\n');
-        }).join('\n@@@\n');
-        showResult(result);
+        }));
     },$("#hide"));
-    $("#hide").append("<br>");
-    $("#hide").append("<br>");
-    var advance = yaju1919.addInputText($("#hide"),{
-        title: "高度な操作",
-        placeholder: colorName.map(function(v,i){
+    yaju1919.addHideArea($("#hide"),{
+        id2: "adv",
+        title: "高度な動き",
+    });
+    var advance = yaju1919.addInputText($("#adv"),{
+        placeholder: colorName.slice(1).map(function(v,i){
             return i + '...' + v;
         }).join('\n'),
         save: "nop",
         hankaku: false,
         textarea: true,
     });
-    var result;
+    function calc(n){
+        var a = n % 8;
+        return a < 5 ? a : 8 - a;
+    }
     addBtn("高度な動きを作成",function(){
-        alert("未完成");
         var ar = getEmojiColors(input()).slice(1);
         var str = advance();
-        result = "@aaa:0.1\n" + ar.concat(ar.slice(1,-1).reverse()).map(function(v,i){
-            var str2 = str;
-            ar.forEach(function(val){
-                str2 = str2.replace(/1/g, ar[0]);
+        showResult(new Array(8).fill().map(function(v,i){
+            return str.replace(/[0-4]/g, function(n){
+                return ar[calc(Number(n) + i)];
             });
-        }).join('\n@@@\n');
-        showResult(result);
-    },$("#hide"));
-    $("#hide").append("<br>");
-    $("#hide").append("<br>");
-    var result_elm = $("<div>").appendTo($("#hide"));
-    function showResult(str){
-        result_elm.empty();
-        str.split('\n').forEach(function(v){
-            $("<div>").text(v).appendTo(result_elm);
-        });
-    }
+        }));
+    },$("#adv"));
     var setup_flag = true;
 })();
